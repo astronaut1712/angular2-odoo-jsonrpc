c1e1193a263591d8174863785cc3d4531dae2b03
Quang <quang.astronaut@gmail.com>
2016-06-29 16:46:01 +0700

    [ADD] Add sourcecode

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # angular2-odoorpc
 OdooRPC for angular2
+
+Migrate from [https://github.com/akretion/angular-odoo/blob/master/src/components/odoo/jsonRpc-service.js](https://github.com/akretion/angular-odoo/blob/master/src/components/odoo/jsonRpc-service.js)
++ Support Basic Authentication
+
+## Functions list
+
+- `login(db, user, pass)`
+- `logout(force)`
+- `getDbList() // doesn't work with odoo >= 9.0`
+- `searchRead(model, domain, fields)`
+- `call(model, method, args, kwargs)`
+
+
+## How to use
+
+Import `OdooRPCService` into component
+
+`import { OdooRPCService } from './odoorpc.service'`
+
+Add provider in app component
+
+```typescript
+@Component({
+    ...
+    providers: [OdooRPCService]
+})
+```
+
+Initialze configuration in constructor of component
+
+```typescript
+
+export class OdooClientExampleComponent {
+
+    constructor(odooRPC: OdooRPCService){
+        this.odooRPC.init({
+            odoo_server: "https://odoo-server-example",
+            http_auth: "username:password" // optional
+        });
+        this.odoo.RPC.login('db_example', 'username', 'password').then(res => {
+            console.log('login success');
+        }).catch( err => {
+            console.error('login failed', err);
+        })
+    }
+
+}
+
+```
---
+++ odoorpc.service.ts
@@ -0,0 +1,220 @@
+import { Injectable } from "@angular/core";
+import { Http, Response, Headers } from "@angular/http";
+
+import "rxjs/add/operator/toPromise";
+
+class Cookies { // cookies doesn't work with Android default browser / Ionic
+
+    private session_id = null;
+
+    delete_sessionId() {
+        this.session_id = null;
+        document.cookie = "session_id=; expires=Wed, 29 Jun 2016 00:00:00 UTC";
+    }
+
+    get_sessionId() {
+        return document
+                .cookie.split('; ')
+                .filter(x => { return x.indexOf('session_id') === 0; })
+                .map(x => { return x.split('=')[1]; })
+                .pop() || this.session_id || "";
+    }
+
+    set_sessionId(val) {
+        document.cookie = `session_id=${val}`;
+        this.session_id = val;
+    }
+}
+
+@Injectable()
+export class OdooRPCService {
+    private odoo_server: string;
+    private http_auth: string;
+    private cookies: Cookies;
+    private uniq_id_counter: number = 0;
+    private shouldManageSessionId: boolean = false; //try without first
+    private context: Object = {"lang": "en_US"};
+    private headers: Headers;
+
+    constructor(
+        private http: Http){
+        this.cookies = new Cookies();
+    }
+
+    private buildRequest(url:string, params:any) {
+        this.uniq_id_counter += 1;
+        if (this.shouldManageSessionId) {
+            params.session_id = this.cookies.get_sessionId();
+        }
+
+        let json_data = {
+            jsonrpc: "2.0",
+            method: "call",
+            params: params, //payload
+        };
+        this.headers = new Headers({
+            "Content-Type": "application/json",
+            "X-Openerp-Session-Id": this.cookies.get_sessionId(),
+            "Authorization": "Basic " + btoa(`${this.http_auth}`)
+        });
+        return JSON.stringify({
+            jsonrpc: "2.0",
+            method: "call",
+            params: params, //payload
+        });
+    }
+
+    private handleOdooErrors(response:any) {
+        response = response.json();
+        if (!response.error) {
+            console.log(response.result);
+            return response.result;
+        }
+
+        let error = response.error;
+        let errorObj = {
+            title: "    ",
+            message:"",
+            fullTrace: error
+        };
+
+        if (error.code === 200 && error.message === "Odoo Server Error" && error.data.name === "werkzeug.exceptions.NotFound") {
+            errorObj.title = "page_not_found";
+            errorObj.message = "HTTP Error";
+        } else if ( (error.code === 100 && error.message === "Odoo Session Expired") || //v8
+                    (error.code === 300 && error.message === "OpenERP WebClient Error" && error.data.debug.match("SessionExpiredException")) //v7
+                ) {
+                    errorObj.title ="session_expired";
+                    this.cookies.delete_sessionId();
+        } else if ( (error.message === "Odoo Server Error" && /FATAL:  database "(.+)" does not exist/.test(error.data.message))) {
+            errorObj.title = "database_not_found";
+            errorObj.message = error.data.message;
+        } else if ( (error.data.name === "openerp.exceptions.AccessError")) {
+            errorObj.title = "AccessError";
+            errorObj.message = error.data.message;
+        } else {
+            let split = ("" + error.data.fault_code).split("\n")[0].split(" -- ");
+            if (split.length > 1) {
+                error.type = split.shift();
+                error.data.fault_code = error.data.fault_code.substr(error.type.length + 4);
+            }
+
+            if (error.code === 200 && error.type) {
+                errorObj.title = error.type;
+                errorObj.message = error.data.fault_code.replace(/\n/g, "<br />");
+            } else {
+                errorObj.title = error.message;
+                errorObj.message = error.data.debug.replace(/\n/g, "<br />");
+            }
+        }
+        console.log(errorObj);
+        return Promise.reject(errorObj);
+    }
+
+    private handleHttpErrors(error: any) {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
+    }
+
+    init(configs) {
+        this.odoo_server = configs.odoo_server;
+        this.http_auth = configs.http_auth || null;
+    }
+
+    setOdooServer(odoo_server:string) {
+        this.odoo_server = odoo_server;
+    }
+
+    setHttpAuth(http_auth:string) {
+        this.http_auth = http_auth;
+    }
+
+    sendRequest(url:string, params:Object): Promise<any> {
+        let options = this.buildRequest(url, params);
+        return this.http.post(this.odoo_server + url, options, {headers: this.headers})
+            .toPromise()
+            .then(this.handleOdooErrors)
+            .catch(this.handleHttpErrors);
+    }
+
+    getServerInfo() {
+        return this.sendRequest('/web/webclient/version_info', {});
+    }
+
+    getSessionInfo() {
+        return this.sendRequest('/web/session/get_session_info', {});
+    }
+
+    login(db:string, login:string, password:string) {
+        let params = {
+                db : db,
+                login : login,
+                password : password
+            };
+        let $this = this;
+        return this.sendRequest("/web/session/authenticate", params).then(function(result) {
+            if (!result.uid) {
+                $this.cookies.delete_sessionId();
+                return Promise.reject({
+                    title: "wrong_login",
+                    message:"Username and password don't match",
+                    fullTrace: result
+                });
+            }
+            $this.context = result.user_context;
+            $this.cookies.set_sessionId(result.session_id);
+            return result;
+        });
+    }
+
+    isLoggedIn(force: boolean = true) {
+        if (!force){
+            return Promise.resolve(this.cookies.get_sessionId().length > 0);
+        }
+        return this.getSessionInfo().then((result) => {
+            this.cookies.set_sessionId(result.session_id);
+            return !!(result.uid);
+        });
+    }
+
+    logout(force: boolean = true) {
+        this.cookies.delete_sessionId();
+        if (force){
+            return this.getSessionInfo().then((r) => { //get db from sessionInfo
+                if (r.db)
+                    return this.login(r.db, '', '');
+            });
+        }else {
+            return Promise.resolve();
+        }
+    }
+
+    getDbList() { //only use for odoo < 9.0
+        return this.sendRequest('/web/database/get_list', {});
+    }
+
+    searchRead(model, domain, fields) {
+        let params = {
+            model: model,
+            domain: domain,
+            fields: fields
+        }
+        return this.sendRequest('/web/dataset/search_read', params);
+    }
+
+    call(model, method, args, kwargs) {
+
+        kwargs = kwargs || {};
+        kwargs.context = kwargs.context || {};
+        Object.assign(kwargs.context, this.context);
+
+        let params = {
+            model: model,
+            method: method,
+            args: args,
+            kwargs: kwargs,
+        };
+        return this.sendRequest('/web/dataset/call_kw', params);
+    }
+
+}
